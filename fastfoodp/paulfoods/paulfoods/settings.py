616cfecfa8c29242db1a8659fd00f152606681d1
paulhernandez200983 <89218294+paulhernandez200983@users.noreply.github.com>
2024-03-05 23:41:56 -0700

    Update settings.py

--- fastfoodp/paulfoods/paulfoods/settings.py
+++ fastfoodp/paulfoods/paulfoods/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-sv)3$pk78gwy6vqf#pks3po9zl*pw)7d38pokp2x!n)qzv%ye7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*']
 
 
 # Application definition
